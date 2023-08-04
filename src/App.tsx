0255d1d1b43605e75a75e63ed2c1533fa580ae51
SantiZai <santyagozaidan@gmail.com>
2023-08-03 22:10:39 -0300

    remove shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,44 @@ import NotFoundPage from "./components/pages/NotFoundPage";
 import NavBar from "./components/containers/NavBar";
 import Strings from "./components/pages/Strings";
 import SingleString from "./components/pages/SingleString";
-import { ThemeProvider } from "./@/components/theme-provider";
-
 
 const App = () => {
     return (
-        <ThemeProvider>
-            <BrowserRouter>
-                <div className="container-all flex flex-col w-screen h-screen">
-                    <NavBar />
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={<HomePage />}
-                        />
-                        <Route
-                            path="profile"
-                            element={<ProfilePage />}
-                        />
-                        <Route
-                            path="strings"
-                            element={<Strings />}
-                        />
-                        <Route
-                            path="strings/:id"
-                            element={<SingleString />}
-                        />
-                        <Route
-                            path="auth"
-                            element={<AuthPage />}
-                        />
-                        <Route
-                            path="not-found"
-                            element={<NotFoundPage />}
-                        />
-                        <Route
-                            path="*"
-                            element={<Navigate to="not-found" />}
-                        />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </ThemeProvider>
+        <BrowserRouter>
+            <div className="container-all flex flex-col w-screen h-screen">
+                <NavBar />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<HomePage />}
+                    />
+                    <Route
+                        path="profile"
+                        element={<ProfilePage />}
+                    />
+                    <Route
+                        path="strings"
+                        element={<Strings />}
+                    />
+                    <Route
+                        path="strings/:id"
+                        element={<SingleString />}
+                    />
+                    <Route
+                        path="auth"
+                        element={<AuthPage />}
+                    />
+                    <Route
+                        path="not-found"
+                        element={<NotFoundPage />}
+                    />
+                    <Route
+                        path="*"
+                        element={<Navigate to="not-found" />}
+                    />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 };
 
