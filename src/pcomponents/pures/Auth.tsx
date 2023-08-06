8a06a7f4cd5bf57d8ea6e5690f4d193ebb16edd3
SantiZai <santyagozaidan@gmail.com>
2023-08-06 14:37:28 -0300

    form finished

--- src/pcomponents/pures/Auth.tsx
+++ src/pcomponents/pures/Auth.tsx
@@ -1,5 +1,5 @@
 import * as Yup from "yup";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Formik, Field, Form, FormikHelpers } from "formik";
 import { Button } from "@/components/ui/button";
 import ErrorMessageCustom from "./ErrorMessageCustom";
 
@@ -13,40 +13,64 @@ interface Values {
     confirm?: string;
 }
 
-const formSchema = Yup.object().shape({
-    fullname: Yup.string()
-        .min(4, "Mínimo 4 caracteres")
-        .max(50, "Máximo 50 caracteres")
-        .required("Campo requerido"),
-    password: Yup.string()
-        .min(4, "Mínimo 4 caracteres")
-        .required("Campo requerido"),
-    confirm: Yup.string()
-        .test(
-            "password-match",
-            "Las contraseñas no coinciden",
-            function (value) {
-                return this.parent.password === value;
-            }
-        )
-        .required("Campo requerido"),
-});
+const getSchema = (signup: boolean) => {
+    let formSchema = Yup.object().shape({
+        fullname: Yup.string()
+            .min(4, "Mínimo 4 caracteres")
+            .max(50, "Máximo 50 caracteres")
+            .required("Campo requerido"),
+        password: Yup.string()
+            .min(4, "Mínimo 4 caracteres")
+            .required("Campo requerido"),
+    });
+
+    if (signup) {
+        formSchema = formSchema.shape({
+            confirm: Yup.string()
+                .required("Campo requerido")
+                .oneOf([Yup.ref("password")], "Las contraseñas no coinciden"),
+        });
+    }
+
+    return formSchema;
+};
 
 const Auth = (props: Props) => {
-    const handleSubmit = (values: Values) => {
-        console.log(values);
+    const handleSubmit = (
+        values: Values,
+        { setSubmitting }: FormikHelpers<Values>
+    ) => {
+        if (props.signup) {
+            console.log("Creando cuenta", values);
+        } else {
+            console.log("Iniciando sesión", values);
+        }
+        setTimeout(() => {
+            console.log("aa");
+            setSubmitting(false);
+        }, 1000);
+    };
+
+    const initialValues = {
+        fullname: "",
+        password: "",
+        confirm: "",
     };
 
     return (
         <div className="w-3/4 mx-auto">
             <Formik
-                initialValues={{
-                    fullname: "",
-                    password: "",
-                    confirm: "",
-                }}
-                onSubmit={(values) => handleSubmit(values)}
-                validationSchema={formSchema}
+                initialValues={initialValues}
+                onSubmit={
+                    handleSubmit as (
+                        values: Values & { confirm: string },
+                        {
+                            setSubmitting,
+                        }: FormikHelpers<Values & { confirm: string }>
+                    ) => void
+                }
+                validationSchema={getSchema(props.signup)}
+                enableReinitialize
             >
                 {({ touched, errors, isSubmitting }) => (
                     <Form>
@@ -57,9 +81,7 @@ const Auth = (props: Props) => {
                                 type="text"
                             />
                             {errors.fullname && touched.fullname && (
-                                <ErrorMessageCustom
-                                    name="fullname"
-                                />
+                                <ErrorMessageCustom name="fullname" />
                             )}
                         </div>
                         <div className="flex flex-col mb-4">
@@ -69,25 +91,27 @@ const Auth = (props: Props) => {
                                 type="text"
                             />
                             {errors.password && touched.password && (
-                                <ErrorMessageCustom
-                                    name="password"
-                                />
+                                <ErrorMessageCustom name="password" />
                             )}
                         </div>
-                        <div className="flex flex-col mb-4">
-                            <label htmlFor="confirm">Confirm: </label>
-                            <Field
-                                name="confirm"
-                                type="text"
-                            />
-                            {errors.confirm && touched.confirm && (
-                                <ErrorMessageCustom
+                        {props.signup && (
+                            <div className="flex flex-col mb-4">
+                                <label htmlFor="confirm">Confirm: </label>
+                                <Field
                                     name="confirm"
+                                    type="text"
                                 />
-                            )}
-                        </div>
+                                {errors.confirm && touched.confirm && (
+                                    <ErrorMessageCustom name="confirm" />
+                                )}
+                            </div>
+                        )}
                         <Button type="submit">
-                            {props.signup ? "Registrarse" : "Acceder"}
+                            {isSubmitting
+                                ? "submitting"
+                                : props.signup
+                                ? "Registrarse"
+                                : "Acceder"}
                         </Button>
                     </Form>
                 )}
